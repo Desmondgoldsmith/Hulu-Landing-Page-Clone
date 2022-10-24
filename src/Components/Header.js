3bab574682dc6be7a8247e99b5af6549818aa06a
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-24 13:36:33 +0000

    Building the header 🎸

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,8 +3,11 @@ import './header.css';
 
 function Header() {
   return (
-    <div className = 'Header'>
-    Header
+    <div className = 'Header' >
+      <div className='header_logo'>
+        <img src='https://raw.githubusercontent.com/bradtraversy/hulu-webpage-clone/main/img/logo.png' alt='hulu logo'/>
+        <button>LOG IN</button>
+      </div>
     </div>
   )
 }
