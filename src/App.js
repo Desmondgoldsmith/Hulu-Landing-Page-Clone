2fc11a0323fb3e9d793f235d22efa671dfa6785a
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-24 13:26:03 +0000

    Init 🐸

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Header from './Components/Header';
 
 function App() {
   return (
     <div className="App">
-       <h1>Big Head</h1>
-    </div>
+       {/* header */}
+       <Header />
+     </div>
   );
 }
 
