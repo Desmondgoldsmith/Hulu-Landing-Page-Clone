e144f2cbda744555f6312f65582efa0f4823fdc7
Desmondgoldsmith <desmondgoldsmith07@gmail.com>
2022-10-25 23:01:37 +0000

    Adding the trending section 💀

--- src/Components/Trending.js
+++ src/Components/Trending.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
-import "react-alice-carousel/lib/alice-carousel.css";
+
 const handleDragStart = (e) => e.preventDefault();
 
 const items = [
-  <img src="path-to-img" onDragStart={handleDragStart} role="presentation" />,
-  <img src="path-to-img" onDragStart={handleDragStart} role="presentation" />,
-  <img src="path-to-img" onDragStart={handleDragStart} role="presentation" />,
+  <img src="https://raw.githubusercontent.com/bradtraversy/hulu-webpage-clone/main/img/logo.png" onDragStart={handleDragStart} role="presentation" />,
+  <img src="https://raw.githubusercontent.com/bradtraversy/hulu-webpage-clone/main/img/logo.png" onDragStart={handleDragStart} role="presentation" />,
+  <img src="https://raw.githubusercontent.com/bradtraversy/hulu-webpage-clone/main/img/logo.png" onDragStart={handleDragStart} role="presentation" />,
 ];
 
 const Trending = () => {
